054279c52518ea8cfb960b3a409c32e14b48c256
Kondor <kondor@revollat.(none)>
2013-04-30 14:57:25 +0200

    yat

--- web/index.php
+++ web/index.php
@@ -1,2 +1,23 @@
 <?php
-echo "test";
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$container = Yolo\createContainer(
+    [
+        'debug' => true,
+    ],
+    [
+        new Yolo\DependencyInjection\MonologExtension(),
+    ]
+);
+
+$app = new Yolo\Application($container);
+
+$app->get('/', function (Request $request) {
+    return new Response("Hallo welt, got swag yo!\n");
+});
+
+$app->run();
